4e825652a86f2a6f79c0ae69176db820d9400571
Rubber Duck NFK <admin@rubberducknfk.com>
2014-05-19 18:33:12 -0400

    adding front at another endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/front', function(req, res) {
+    res.render('front', { title: 'Express' });
+});
+
 module.exports = router;
