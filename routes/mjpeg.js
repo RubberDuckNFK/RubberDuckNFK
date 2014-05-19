0ed37aff5ee841613b0e7ac9c219f9d0960223f7
Rubber Duck NFK <admin@rubberducknfk.com>
2014-05-19 18:23:43 -0400

    Adding camera #2

--- routes/mjpeg.js
+++ routes/mjpeg.js
@@ -6,6 +6,6 @@ router.get('/index1.jpg',
     new MjpegProxy('http://72.214.52.227:49152/mjpg/video.mjpg').proxyRequest);
 
 router.get('/index2.jpg',
-    new MjpegProxy('http://23.253.149.209:8000/').proxyRequest);
+    new MjpegProxy('http://23.253.149.209:8000').proxyRequest);
 
 module.exports = router;
