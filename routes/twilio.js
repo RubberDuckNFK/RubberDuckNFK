ae6dbdf93cfb4eaee9a13a4d4e8d0d69463bff85
Rubber Duck NFK <admin@rubberducknfk.com>
2014-05-22 14:48:41 -0400

    Final SMS

--- routes/twilio.js
+++ routes/twilio.js
@@ -26,27 +26,26 @@ router.post('/respondToSms', function(req, res) {
     if (twilio.validateExpressRequest(req, '7580cfb76678516499711f7ceb9aaf7a')) {
 
 
-    FB.setAccessToken('CAAG1dr42JNwBAGVmjCJblVgZCQVRDvM8zBcTTOSoLfDno5cD1pPQxQbZBsZAqnuGaCKr0F31QW1SXlELetxFEDu9Ch0kCj6OrpB9bHrTIX0LZBUSOLpu61o7OFqaV6bw2mmAZBMhyJDwp4sNbVaczOZA0ZCg1LrqaqhZC6mWSRdLo4YYGWrtjhKD');
 
-    FB.api(
+        FB.setAccessToken('CAAG1dr42JNwBAGVmjCJblVgZCQVRDvM8zBcTTOSoLfDno5cD1pPQxQbZBsZAqnuGaCKr0F31QW1SXlELetxFEDu9Ch0kCj6OrpB9bHrTIX0LZBUSOLpu61o7OFqaV6bw2mmAZBMhyJDwp4sNbVaczOZA0ZCg1LrqaqhZC6mWSRdLo4YYGWrtjhKD');
+
+        FB.api(
             "/1414766195467290/photos",
             "POST",
             {
-
                 "url": "http://rubberducknfk.com/images/front.jpg",
-                "place" : "8070729884",
-                "message" : "I just took someone's picture! They texted '" + req.body + "' to me at (920) 75DUCKY !  #chryslerquack "
+                "place": "8070729884",
+                "message": "I just took someone's picture! Get yours by texting (920) 75DUCKY!  #chryslerquack "
             },
             function (response) {
-                    var twiml = new twilio.TwimlResponse();
+                var twiml = new twilio.TwimlResponse();
 
-                    twiml.sms('Thanks for texting the Rubber Duck! I just posted a picture from the webcam! Tag yourself at ' +
-                        'https://www.facebook.com/photo.php?fbid=' + response.id);
+                twiml.sms('Thanks for texting the Rubber Duck! I just posted a picture from the webcam! Tag yourself at ' +
+                    'https://www.facebook.com/photo.php?fbid=' + response.id);
 
-                    res.type('text/xml');
-                    res.send(twiml.toString());
+                res.type('text/xml');
+                res.send(twiml.toString());
             });
-
     }
     else {
         res.send('you are not twilio.  Buzz off.');
